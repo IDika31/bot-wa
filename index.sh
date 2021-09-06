25c8fd0e6c24b838151ddbc5d4f5df3694f332c8
IDika <andikanugraha310706@gmail.com>
2021-09-06 11:29:44 +0800

    Commit pertama

--- index.sh
+++ index.sh
@@ -5,7 +5,6 @@ apt-get install git
 apt-get install wget
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-git clone 
 npm install
 
 echo "[*] All dependencies have been installed, running the bot..."
